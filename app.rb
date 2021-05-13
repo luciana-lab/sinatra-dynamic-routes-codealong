e4f175e66726d8d702f16a328bd39bca988d6f05
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-13 17:34:31 -0400

    commit tree ebfd15b9384ede5a97c09c208a4d6e2d476a2726

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -10,9 +11,25 @@ class App < Sinatra::Base
   # This is a sample dynamic route.
   get "/hello/:name" do
     @user_name = params[:name]
-    "Hello #{@user_name}!"
+    "Hello #{@user_name.capitalize}!"
   end
 
   # Code your final two routes here:
+
+  # sends a 200 status code
+  # displays 'goodbye' and a name
+  # is not hard-coded
+  get '/goodbye/:name' do
+    @person_name = params[:name]
+    "Goodbye, #{@person_name}."
+  end
+
+  # sends a 200 status code
+  # displays the product of the two numbers in the route
+  # is not hard-coded
+  get '/multiply/:num1/:num2' do
+    @number = params[:num1].to_i * params[:num2].to_i
+    "#{@number}"
+  end
 
 end
